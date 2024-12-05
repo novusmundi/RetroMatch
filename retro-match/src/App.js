f69416e48a9e7d42feb4c1faf834a35982d93deb
AdrianaP00 <adrianapiccolo00@gmail.com>
2024-12-05 12:37:40 +0100

    Add SwipeButton component and styles for swipe functionality

--- retro-match/src/App.js
+++ retro-match/src/App.js
@@ -3,16 +3,18 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import CardContainer from './components/CardContainer';
+import SwipeButton from './components/SwipeButton';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <CardContainer />
-      </main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <CardContainer />
+            </main>
+            <SwipeButton />
+        </div>
+    );
 }
 
 export default App;
